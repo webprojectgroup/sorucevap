7608e3b01024a3d8a6e3cb0375bff881e7c4c071
mustafairan <mustafiran@gmail.com>
2014-07-21 04:55:36 +0300

    fonksiyonlar tamamlandı

--- sinavOl.php
+++ sinavOl.php
@@ -3,62 +3,73 @@
 <?php
 include("dbBaglan.php");
 
-//sürenin hesaplanmasý
+//sürenin hesaplanmasý.
 
 $soruSayisi=$_POST['soruSayisi'];
 $zorluk=$_POST['zorluk'];
 $kategori=$_POST['kategori'];
-// aldýgý tablonýn o kolonundaki en büyük deðeri dondürecek fonksiyon
-/* 
+
+// aldýgý tablonun o kolonundaki en büyük deðeri dondürenfonksiyon
 function en_buyuk_deger($db_baglanti_degiskeni,$tablo_ismi,$kolon_ismi){
-$sql = mysqli_query($db_baglanti_degiskeni,"SELECT $kolon_ismi FROM $tablo_ismi ORDER BY $kolon_ismi DESC LIMIT 1 ;");
-$satir = mysqli_fetch_array($sql); 
-$en_buyuk_deger=$satir['kolon_ismi'];
+$query= "SELECT MAX(".$kolon_ismi.") FROM " .$tablo_ismi ;
+$sonuc=mysqli_query($db_baglanti_degiskeni,$query);
+$satir=mysqli_fetch_array ($sonuc);
+$en_buyuk_deger=$satir[0];
+return $en_buyuk_deger;
+}
+
+// aldýgý tablonun o kolonundaki en küçük deðerini dondüren fonksiyon
+function en_kucuk_deger($db_baglanti_degiskeni,$tablo_ismi,$kolon_ismi){
+$query= "SELECT MIN(".$kolon_ismi.") FROM ".$tablo_ismi;
+$sonuc=mysqli_query($db_baglanti_degiskeni,$query);
+$satir=mysqli_fetch_array ($sonuc);
+$en_kucuk_deger=$satir[0];
+return $en_kucuk_deger;
+}
+
+ 
+//aldýgý arrayde belli bir deger var mý yok mu onu dondurecek fonksiyon
+function arrayde_varmi($aranacak_deger,$array ){
+if(array_key_exists($array,$aranacak_deger)){return TRUE;}
+else{return FALSE;}
 
 }
-*/
 
-//aldýgý arrayde belli bir deger var mý yok mu onu dondurecek fonksiyon
-/*
-function arrayde_varmi($array, $aranacak_deger){}
-*/
 
 //veritabanýnda verilen kolonda aranacak deger in olup olmadýgýný dondurecek fonksiyon
-/*
-function bu_id_varmi($db_baglanti_degiskeni,$aranacak_deger,$tablo_ismi,$kolon_ismi){} 
-*/
+function bu_varmi($db_baglanti_degiskeni,$aranacak_deger,$tablo_ismi,$kolon_ismi){
+$sorgu="SELECT ".$kolon_ismi ." FROM ".$tablo_ismi. " WHERE ".$kolon_ismi."='".$aranacak_deger ."' LIMIT 1" ;
+$sonuc=mysqli_query($db_baglanti_degiskeni,$sorgu);
+$satir=mysqli_fetch_array ($sonuc);
 
-// aþagýdaki kýsým fonksiyonlar hazýrlanýrken kullanýlabilir . Zaten buyuk oranda o kýsýmda sadece fonksiyon cagýrarak iþi halletmeye calýsacagýz.
-
-$i=1;
-
-$onaylanan_id=array("1");
-
-while ($i<=$soruSayisi){
-
-	$rastgele_sayi=rand(1,$en_buyuk_id);
-
-	$query="SELECT id FROM soru WHERE id=" .$rastgele_sayi;
-	
-	$sql=mysqli_query($db_baglanti_durumu,$query);
-
-	echo "uretilen sayi" .$rastgele_sayi ."<br/>" ; 
-
-	if(mysqli_num_rows($sql)==1){
-	$counter=1;
-	while($counter<=count($onaylanan_id)){
-	$onaylanan[$counter]
-	if
-		$i=$i+1;
-		array_push($onaylanan_id,$rastgele_sayi);
-	}
-	echo "onaylanan". ($i-1)."<br/>";
+if( isset($satir[0])){return TRUE;}
+else{return FALSE;}
 
 }
-		
-				    }				   
-				   foreach ($onaylanan_id as $value) { print " " . $value . " ";}
-echo $onaylanan_id [6];
+//aldýgý tablonun kolonundan istenen sayýda rastgele id degeri uretecek ve bunu array olarak döndürecek
+function rastgele_id($db_baglanti_degiskeni,$tablo_ismi,$kolon_ismi,$istenen_deger_sayisi){
+$rastgele_idler=array();
+$min=en_kucuk_deger($db_baglanti_degiskeni,$tablo_ismi,$kolon_ismi);
+$max=en_buyuk_deger($db_baglanti_degiskeni,$tablo_ismi,$kolon_ismi);
+$counter=1;
+while($counter<=$istenen_deger_sayisi){
+$rastgele_sayi=rand($min,$max); //id aralýgýnda rastgele sayý elde edildi
+if (bu_varmi($db_baglanti_degiskeni,$rastgele_sayi,$tablo_ismi,$kolon_ismi)){
+
+if(!arrayde_varmi($rastgele_idler,$rastgele_sayi)){
+$counter=$counter+1;
+array_push($rastgele_idler,$rastgele_sayi);
+}
+}
+
+else{}
+
+}
+
+return $rastgele_idler;
+}
+ foreach (rastgele_id($db_baglanti_durumu,"soru","id",3) as $value){echo " ". $value ." ";}
+
 ?>
 
 </body>
