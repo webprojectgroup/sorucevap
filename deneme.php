85fd6810aca76ae1a85a737baac725a70b21cfa3
Mustafa iran <mustafiran@gmail.com>
2015-04-03 17:13:28 +0300

    Delete deneme.php

--- deneme.php
+++
@@ -1,9 +0,0 @@
-<form>
-<select name = "zorluk"  >
-  <option value = 1>1</option>
-  <option value = 2>2</option>
-  <option <?php echo "selected"; ?> value = 3>3</option>
-  <option value = 4>4</option>
-  <option value = 5>5</option>
-  </select>
-</form>
